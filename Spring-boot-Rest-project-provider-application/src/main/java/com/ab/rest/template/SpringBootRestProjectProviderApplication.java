d9b8eee3c656745ea5c3213460e057ee11240019
abhisheksharmas04 <abhisheksharmaas04@gmail.com>
2023-05-03 16:09:23 +0530

    Rest-Template provider application created

---
+++ Spring-boot-Rest-project-provider-application/src/main/java/com/ab/rest/template/SpringBootRestProjectProviderApplication.java
@@ -0,0 +1,13 @@
+package com.ab.rest.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProjectProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProjectProviderApplication.class, args);
+	}
+
+}
